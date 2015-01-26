0cdad3cb2a08578f4004d7f82a02c1e30ca5b358
Henesy <henesy.dev@gmail.com>
2015-01-26 00:19:48 -0600

    Adding python hello and updating python README.txt

---
+++ Python/README.txt
@@ -0,0 +1,2 @@
+All files in this directory should be treated as Python 3.x and should be operable with soley "python".
+
---
+++ Python/hello.py
@@ -0,0 +1,8 @@
+usrname = input("What is your name?: ")
+print("Hello,", usrname, "!")
+
+a = 2
+b = 2
+c = a + b
+
+print("2 plus 2 is: ", c, "!")
