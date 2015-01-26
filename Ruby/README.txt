5cefe355dcf3e6dacfd8e6f6a637f0d0384a44e1
Henesy <henesy.dev@gmail.com>
2015-01-26 00:26:20 -0600

    Adding Ruby hello and updating Ruby README.txt

---
+++ Ruby/README.txt
@@ -0,0 +1,3 @@
+All '.rb' files in this directory should be assumed to be Ruby 2.x unless specified otherwise.
+
+
---
+++ Ruby/hello.rb
@@ -0,0 +1,12 @@
+def main()
+	print "What is your name?: "
+	$usrname = $stdin.gets.chomp
+	print "Hello, #{$usrname}!\n"
+
+	$a = 2
+	$b = 2
+	$c = $a + $b
+	print "2 plus 2 equals: #{$c}!\n"
+end
+
+main()
