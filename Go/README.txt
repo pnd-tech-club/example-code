afb81d22bf5128f75e58a0dc1b858d7488cd5cac
Henesy <henesy.dev@gmail.com>
2015-01-26 00:04:07 -0600

    Adding Go hello program

---
+++ Go/README.txt
@@ -0,0 +1,1 @@
+All '.go' files in this directory should be operable using "go run file.go" unless specified otherwise.
---
+++ Go/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+"fmt"
+)
+
+func main() {
+	var a, b int
+	usrname := ""
+
+	fmt.Print("What's your name?: ")
+	fmt.Scan(&usrname)
+	fmt.Println("Hello, ", usrname, "!")
+
+	a, b = 2, 2
+	c := a + b
+	fmt.Println("Two plus two is: ", c)
+}
