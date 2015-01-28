8075eee97ace68abf70eb46b460d45c0654e6c17
Sean Hinchee <henesy@Agamemnon.local>
2015-01-28 16:53:35 -0600

    Updated hello.hs to bring it up to standard Haskell: [x]

--- Haskell/hello.hs
+++ Haskell/hello.hs
@@ -1,7 +1,16 @@
 word :: String 
 word = "world"
-hworld :: String
-hworld = "Hello " ++ word
 
-main = print hworld
+a,b,c :: Int
+a = 2
+b = 2
+c = a + b
+
+main = do 
+	putStrLn "What's your name? "
+	usrname <- getLine
+	print "Hello"	
+	putStrLn ("Hello " ++ usrname)
+	putStrLn "2 plus 2 is: "
+	print c
 
