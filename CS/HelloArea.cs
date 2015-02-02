1afb52bf242d1d301edcf92881841fec8dea25e9
Sean Hinchee <henesy.dev@gmail.com>
2015-02-02 15:16:43 -0600

    Adding HelloArea.cs
    Not sure if this file is functional, will test later, flag for
    UNTESTED. ~Sean

---
+++ CS/HelloArea.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Hello_World
+{
+	class MainClass
+	{
+		public static void getArea() {
+			const double pi = 3.14159; //This is a constant
+
+			double radius; //Declare radius and area variables
+			double area;
+
+			Console.WriteLine("Enter Radius: ");
+			radius = Convert.ToDouble(Console.ReadLine());
+			area = pi * radius * radius;
+
+			Console.WriteLine("Radius: {0}, Area: {1}", radius, area);
+			Console.ReadLine();
+		}
+
+		public static void Main (string[] args)
+		{
+			//Single line comment
+
+			/*Multi
+			 * line
+			 * comment
+			 */
+
+			Console.WriteLine ("Hello World!");
+
+			getArea();
+		}
+	}
+}
